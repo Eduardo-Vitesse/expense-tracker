a2a42c31a6bf0135ada46dab4b843bb90731556e
Luiz Eduardo F S <edu.dev.py@gmail.com>
2023-04-25 23:50:10 -0300

    first commit

---
+++ lib/screens/expense_screen.dart
@@ -0,0 +1,40 @@
+import "package:flutter/material.dart";
+
+import "package:expense_tracker/models/expense.dart";
+
+class ExpenseScreen extends StatefulWidget {
+  const ExpenseScreen({super.key});
+
+  @override
+  State<ExpenseScreen> createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter course",
+      amount: 99.19,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.17,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text("The Chart"),
+          Text("Expense List"),
+        ],
+      ),
+    );
+    ;
+  }
+}
